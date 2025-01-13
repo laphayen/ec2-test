72a1011a9e2c3729edae241d146c2217f61a5656
Baek Gi Chan <laphayen@gmail.com>
2025-01-14 02:09:04 +0900

    Add a Spring Boot project for EC2 testing

---
+++ src/main/java/com/laphayen/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.laphayen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/laphayen/Ec2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laphayen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
